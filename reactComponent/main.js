b0fc008413ad467a01f1afe79678b50710163158
Adil Sulaiman <asulaiman@theworkinggroup.ca>
2019-02-26 15:54:24 -0500

    update bundle

--- reactComponent/main.js
+++ reactComponent/main.js
@@ -105,7 +105,7 @@ var _App2 = _interopRequireDefault(_App);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_reactDom2.default.render(_react2.default.createElement(_App2.default, { showGoogleMap: true }), document.body);
+_reactDom2.default.render(_react2.default.createElement(_App2.default, null), document.body);
 
 /***/ }),
 /* 1 */
